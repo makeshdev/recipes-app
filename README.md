004108b436059b98c58c41addd226eb8d160962a
Makesh <126241615+makeshdev@users.noreply.github.com>
2025-02-16 12:21:19 +0530

    app structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# Recipes-app-api
+
+Simple api CURD functionality using node-express-mongoDB
+
+### important commands
+
+1. `npm install`
+2. `npm run server`
+3. `git init`
+4. `git add .`
+5. `git commit -m "msg"`
+6. `git push -u orgin main` or` git push`
--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const dotenv = require("dotenv");
 const path = require("path");
 dotenv.config({ path: path.join(__dirname, "config", "config.env") });
 
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "Hello Node JS",
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(
     `server running on port ${process.env.PORT} to ${process.env.NODE_ENV}`
